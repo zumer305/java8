eb2bc18f797a6ef514532bf0c83c369e7452aa29
zumer305 <zumerniaz305@gmail.com>
2025-09-18 02:46:05 +0700

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+let arr=[1,2,3,4,5];
+// function
+arr.forEach(function(el){
+    console.log(el);
+});
+// arrow function
+arr.forEach((el)=>{
+    console.log(el);
+});
+
+// OR
+
+// let print=function(el){
+//     console.log(el);
+
+// };
+// arr.forEach(print);
+
+
